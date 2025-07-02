6b5d9eb25985511b417923438dbe2529882d37b5
GOJOSATORU63 <lamanarendra981@gmail.com>
2025-07-02 17:03:36 +1000

    Adding the Express Router

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const User = require("./models/user");
 const connectDB = require("./config/database");
 const app = express();
-const { validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
+
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const { userAuth } = require("./middlewares/auth");
@@ -11,86 +10,15 @@ const { userAuth } = require("./middlewares/auth");
 app.use(express.json());
 app.use(cookieParser());
 
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+
 //Adding the user to the database
-app.post("/signup", async (req, res) => {
-  try {
-    //Validation of data
-    validateSignUpData(req);
-
-    const { firstName, lastName, emailId, password } = req.body;
-
-    //Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash);
-
-    // Creating a new instance of the User model
-
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-    });
-
-    await user.save();
-    res.send("User Added sucessfully!");
-  } catch (err) {
-    res.status(400).send("Error: " + err.message);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-
-    const user = await User.findOne({ emailId: emailId });
-
-    if (!user) {
-      throw new Error("EmailId id not present in DB");
-    }
-
-    // const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    const isPasswordValid = await user.validatePassword(password);
-
-    if (isPasswordValid) {
-      //Create a JWT Token
-
-      const token = await user.getJWT();
-
-      //Add the token to cookie and send the response back to the user
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
-      });
-      res.send("Login Sucessful!!!");
-    } else {
-      throw new Error("Password is not correct");
-    }
-  } catch (err) {
-    res.status(400).send("ERROR Fuck this shit: " + err.message);
-  }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-
-    if (!user) {
-      throw new Error("User does not exist");
-    }
-
-    res.send(user);
-  } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
-  }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  //Sendint the Connection Request
-  console.log("Sending a connection request");
-  res.send(user.firstName + " sent the connect request!");
-});
 
 //Get user by email
 app.get("/user", async (req, res) => {
---
+++ src/routes/auth.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+
+const { validateSignUpData } = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    //Validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    //Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    // Creating a new instance of the User model
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User Added sucessfully!");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("EmailId id not present in DB");
+    }
+
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      //Create a JWT Token
+
+      const token = await user.getJWT();
+
+      //Add the token to cookie and send the response back to the user
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
+      });
+      res.send("Login Sucessful!!!");
+    } else {
+      throw new Error("Password is not correct");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR Fuck this shit: " + err.message);
+  }
+});
+
+module.exports = authRouter;
