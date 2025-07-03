10ae76c7b6e624e6e1cf7c641e4cbf6589ec32ed
GOJOSATORU63 <lamanarendra981@gmail.com>
2025-07-03 17:52:52 +1000

    Added the logout + edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,7 +3,9 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+const { validateEditProfileData } = require("../utils/validation");
+
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
 
@@ -12,6 +14,27 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     }
 
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+    console.log(loggedInUser);
+    res.json({
+      message: `${loggedInUser.firstName}, your profile updated sucessfully`,
+      data: loggedInUser,
+    });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
